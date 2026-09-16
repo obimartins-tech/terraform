32694deee6347027020a61fe08241f6dfe4267aa
obimartins-tech <obimartins.tech@gmail.com>
2026-09-16 06:52:36 +0000

    Terraform apply done, and all resources created and running

--- class-work/network.tf
+++ class-work/network.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.20.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
